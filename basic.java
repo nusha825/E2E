996fb46bd70499b6ffdcf0b31949382f18c6680c
Nusha AGF <160996531+nusha825@users.noreply.github.com>
2025-02-08 17:55:52 +0530

    remote changes done

--- basic.java
+++ basic.java
@@ -1,7 +1,9 @@
 class basic
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         System.out.println("Programming is Fun!");
+        System.out.println("I'am Nusha");
     }
     
 }
